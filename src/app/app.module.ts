5f187e4812f133de1a08583c4c0b242dea9faccf
liviagoncalves <liviagoncalves.morais@gmail.com>
2022-06-01 17:50:20 -0300

    Criação filters, footer, header, nav, product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { FiltersComponent } from './components/bookstore-app/filters/filters.component';
+import { ProductListComponent } from './components/bookstore-app/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    FiltersComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
